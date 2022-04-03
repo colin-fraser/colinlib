2df5c9436c3eb56c2602fae20fc2b064436a5c06
Colin Fraser <colinfraser@fb.com>
2022-04-03 14:09:46 -0700

    Added slice_top_categories

--- tests/testthat/test-slice-dice.R
+++ tests/testthat/test-slice-dice.R
@@ -1,15 +1,37 @@
+df <- dplyr::tribble(
+  ~a, ~b, ~c,
+  "A", "A", 1,
+  "A", "A", 1,
+  "A", "A", 1,
+  "A", "B", 1,
+  "A", "B", 1,
+  "A", "C", 1,
+  "B", "A", 1,
+  "B", "B", 1,
+  "C", "A", 10,
+  "D", "A", 10
+)
 test_that("slice_top_n", {
-  actual <- ggplot2::diamonds %>%
-    slice_top_categories(cut, .n=2)
-  expected <- ggplot2::diamonds %>%
-    filter(cut %in% c('Ideal', 'Premium'))
+  actual <- df %>%
+    slice_top_categories(a, .n=2)
+  expected <- df %>%
+    dplyr::filter(a %in% c('A', 'B'))
   expect_equal(actual, expected)
 })
 
 test_that("slice_top_n with weight", {
-  actual <- ggplot2::diamonds %>%
-    slice_top_categories(cut, .n=2, .wt = -carat)
-  expected <- ggplot2::diamonds %>%
-    filter(cut %in% c('Fair', 'Good'))
+  actual <- df %>%
+    slice_top_categories(a, .n = 2, .wt = c)
+  expected <- df %>%
+    dplyr::filter(a %in% c('C', 'D'))
   expect_equal(actual, expected)
 })
+
+test_that("slice_top_n multiple cols", {
+  actual <- df %>%
+    slice_top_categories(a, b, .n = 2)
+  expected <- df %>%
+    dplyr::filter(a == "A", b %in% c('A', 'B'))
+  expect_equal(actual, expected)
+}
+)
