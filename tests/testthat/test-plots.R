a25d01553f916c22c9ac3cd92998cc89adb66d9c
Colin Fraser <colinfraser@fb.com>
2022-04-04 08:24:57 -0700

    also fixes #3

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -11,3 +11,25 @@ test_that("stat_annotate_", {
       stat_annotate_min()
   )
 })
+
+test_that("guessing labeler works", {
+  df <- palmerpenguins::penguins %>%
+    dplyr::count(year, species, island, sex)
+
+  vdiffr::expect_doppelganger(
+    "test_guess_factor_label",
+    ggplot(df, aes(x = year, y = n, color = species, linetype = sex)) +
+    facet_wrap(vars(island)) +
+    geom_line() +
+    stat_annotate_first(aes(annotation_label = species))
+  )
+
+  vdiffr::expect_doppelganger(
+    "test_guess_char_label",
+    ggplot(dplyr::mutate(df, species = as.character(species)),
+           aes(x = year, y = n, color = species, linetype = sex)) +
+      facet_wrap(vars(island)) +
+      geom_line() +
+      stat_annotate_first(aes(annotation_label = species))
+  )
+})
