aa818e5a6922c07b430a3dba3ecbc8d674034861
Colin Fraser <colinfraser@fb.com>
2022-05-04 12:33:04 -0700

    updates to obsidian cli thing

--- R/obs_cli.R
+++ R/obs_cli.R
@@ -40,16 +40,15 @@ parse_year <- function(x) {
 }
 
 short_cite <- function(x) {
-  authors <- parse_authors(x)
+  author <- stringr::str_extract(parse_authors(x)[1], "[\\w-']+")
   year <- parse_year(x)
-  glue::glue("{authors[1]} ({year})")
+  glue::glue("{author} ({year})")
 }
 
 sanitize_title <- function(x) {
   stringr::str_replace_all(x, "[:]", "-")
 }
 
-<<<<<<< HEAD
 to_link <- function(x) {
   paste0("[[", x, "]]")
 }
@@ -58,25 +57,22 @@ to_md_list <- function(x) {
   paste0("- ", x, collapse = "\n")
 }
 
-=======
->>>>>>> ca44081eeea915ea09728ec77f39bdb3c77d82de
-new_paper <- function(x, extra_tags = NULL, directory = "notes") {
+to_tag <- function(x) {
+  paste0("#", x)
+}
+
+new_paper_content <- function(x, extra_tags) {
   template <- "
   ---
   aliases: [{title}, {short_cite(x)}]
-  tags: [{tags}]
   ---
   # {title}
-<<<<<<< HEAD
   **Citation**: {x}
   **Status**: #to-read
   **Authors**:
   {authors}
-=======
-
-  **Citation**: {x}
-  **Status**: #to-read
->>>>>>> ca44081eeea915ea09728ec77f39bdb3c77d82de
+  **Tags**:
+  {tags}
 
   ## Abstract
 
@@ -89,11 +85,33 @@ new_paper <- function(x, extra_tags = NULL, directory = "notes") {
   authors <- parse_authors(x) %>%
     to_link() %>%
     to_md_list()
-
   year <- parse_year(x)
+  aliases <- short_cite(x)
+  tags <- paste(c("paper", extra_tags)) %>%
+    to_tag() %>%
+    to_md_list()
+  glue::glue(template)
+}
 
-  aliases <- short_cite
-  tags <- paste(c("paper", extra_tags), collapse = ", ")
+new_paper_uri <- function(x, extra_tags = NULL, directory = "notes") {
+  title <- parse_title(x)
+  content <- new_paper_content(x, extra_tags)
   obs_uri('new', name = fs::path_join(c(directory, sanitize_title(title))),
-          content=urltools::url_encode(glue::glue(template)))
+          content=urltools::url_encode(content))
+}
+
+
+#' Create a paper entry
+#'
+#' @param x the citation in Vancouver style
+#' @param extra_tags extra tags
+#' @param directory directory to create the notes
+#'
+#' @return NULL
+#' @export
+#'
+new_paper <- function(x, extra_tags = NULL, directory = "notes", browse = TRUE) {
+  uri <- new_paper_uri(x, extra_tags, directory)
+  if (browse) browseURL(uri)
+  uri
 }
